3608ea66c61d83ac126cbb86d5459dbde03f6faa
Quin <quinb96@protonmail.com>
2020-05-23 18:27:23 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name="tkAudioConverter",
+	version="1.0",
+	description="A password manager coded in python3 using Tkinter.",
+	url="https://github.com/sketchyboi14/",
+	author="Quin Brown",
+	author_email="quinb96@protonmail.com",
+	include_package_data=True,
+	packages=find_packages(),
+	platforms="Linux",
+	zip_safe=False) 
