44decdf8e60040315ebc12fb54051d52b529fe99
Quin <quinb96@protonmail.com>
2020-07-23 16:18:08 -0700

    Revised

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from sys import platform
+
+setup(name="tkAudioConverter",
+      author="Quin Brown",
+      author_email="quinb96@protonmail.com",
+      version="1.0",
+      description="A gui audio converter that converts the most common audio formats.",
+      install_requires=["pydub", "ttkthemes"],
+      url="https://github.com/sketchyboi14/tkAudioConverter",
+      platforms=["Linux", "Windows"],
+      include_package_data=True,
+      zip_safe=False,
+      packeges=find_packages(),
+      )
+        
